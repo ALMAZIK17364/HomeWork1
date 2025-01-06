b2001b59237214d82a5d8493ac21c731abd291a5
MrAll <81728811+ALMAZIK17364@users.noreply.github.com>
2025-01-06 14:01:17 +0800

    m

---
+++ main.py
@@ -0,0 +1,38 @@
+#1st program
+
+result = (9 ** 0.5) * 5
+print(result)
+
+#2nd program
+
+if (9.99 > 9.98 and 1000) and 9.99 != 1000.1:
+    print(True)
+else:
+    print(False)
+
+#3rd program
+
+result1 = 2+2*2
+result2 = (2+2)*2
+
+print(result1)
+print(result2)
+
+if result1 == result2:
+    print(True)
+else:
+    print(False)
+
+#4th program
+
+_str = "123.456"
+
+_float = float(_str)
+
+_float = _float * 10
+
+_int = int(_float)
+
+result = _int%10
+
+print(result)
